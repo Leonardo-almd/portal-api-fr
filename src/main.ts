421b1ec33c4704f886eaed58c6db667bf4a5259c
Leonardo Almeida <leonardo.alv1@hotmail.com>
2025-01-22 17:02:21 -0300

    add-Logger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { PermissionSeeder } from './seeders/permission.seeder';
 import { DataSource } from 'typeorm';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.useLogger(new Logger());
   app.enableCors();
 
   // Obtendo o DataSource diretamente do NestJS
